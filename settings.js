b9988cd77d3f267270d0d5e1c1b13d13b9c60e5d
Captain-devs <162444918+Captain-devs@users.noreply.github.com>
2025-04-24 20:27:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  botName: "CAPTAIN Bot",
+  botOwner: 'CAPTAIN', // Your name
+  ownerNumber: '2348104399603', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
